42de682507e4c79c7ed6c8dc8499c36a4f87eec2
Wesley Ricarte <wesley.ricarte97@gmail.com>
2022-09-22 03:09:13 -0300

    finished

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,18 @@
-/* Desenvolva seu código aqui... */
+const buttonsControllersModal = document.querySelectorAll(
+  "[data-modal-control]"
+);
+
+// console.log(buttonsControllersModal);
+
+for (let i = 0; i < buttonsControllersModal.length; i++) {
+  buttonsControllersModal[i].addEventListener("click", function () {
+    // Event.perventDefault();
+    let valueDataModalControl =
+      buttonsControllersModal[i].getAttribute("data-modal-control");
+
+    console.log(valueDataModalControl);
+    document
+      .getElementById(valueDataModalControl)
+      .classList.toggle("show-modal");
+  });
+}
